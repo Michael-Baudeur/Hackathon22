313590bb9759e43c6c70f33c67fed346aee7ec6e
Michael-Baudeur <michael.baudeur@gmail.com>
2022-05-15 04:02:25 +0200

    Which Fucking Bin v0.2 Get value from url but not real data

---
+++ app/src/main/java/api/ApiService.kt
@@ -0,0 +1,22 @@
+package api
+
+import api.models.Comment
+import api.models.Post
+import api.models.User
+import retrofit2.Response
+import retrofit2.http.*
+
+interface ApiService {
+
+    @GET("users")
+    suspend fun getUsers(): Response<MutableList<User>>
+
+    @GET("posts/{num}")
+    suspend fun getPostById(@Path("num") num : Int): Response<Post>
+
+    @GET("comments")
+    suspend fun getCommentsByPost(@Query("postId") postId : Int): Response<MutableList<Comment>>
+
+    @POST("posts")
+    suspend fun createPost(@Body post: Post): Response<Post>
+}
--- app/src/main/java/com/google/mlkit/samples/codescanner/kotlin/MainActivity.kt
+++ app/src/main/java/com/google/mlkit/samples/codescanner/kotlin/MainActivity.kt
@@ -16,20 +16,126 @@
 
 package com.google.mlkit.samples.codescanner.kotlin
 
+/*import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.longToast
+import org.jetbrains.anko.uiThread*/
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.util.Log
 import android.view.View
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.google.mlkit.common.MlKitException
 import com.google.mlkit.samples.codescanner.R
 import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.codescanner.GmsBarcodeScanning
-import java.util.Locale
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.net.MalformedURLException
+import java.net.URL
+import java.util.*
+
 
 /** Demonstrates the code scanner powered by Google Play Services. */
 class MainActivity : AppCompatActivity() {
   var barCodeValue: String = ""
+  var product_data: String = ""
 
+  //API code
+  /*private fun executeCall() {
+    launch(Dispatchers.Main) {
+      try {
+        val response = ApiClient.apiService.getPostById(1)
+
+        if (response.isSuccessful && response.body() != null) {
+          val content = response.body()
+  //do something
+        } else {
+          Toast.makeText(
+            this@MainActivity,
+            "Error Occurred: ${response.message()}",
+            Toast.LENGTH_LONG
+          ).show()
+        }
+
+      } catch (e: Exception) {
+        Toast.makeText(
+          this@MainActivity,
+          "Error Occurred: ${e.message}",
+          Toast.LENGTH_LONG
+        ).show()
+      }
+    }
+  }*/
+  //--------------------------
+
+  fun get_product_data(barCode: String): String {
+    val productURL = "https://world.openfoodfacts.org/api/v0/product/$barCode.json"
+    //val productURL = "https://pastebin.com/raw/2bW31yqa"
+    var apiResponse = ""
+
+    try {
+      if(isNetworkConnected()){
+        val url: URL? = try {
+          URL(productURL)
+        }catch (e: MalformedURLException){
+          Log.d("Exception", e.toString())
+          null
+        }
+        url?.let { findViewById<TextView>(R.id.products_data).text = it.toString() }
+
+        // io dispatcher for networking operation
+        lifecycleScope.launch(Dispatchers.IO) {
+          url?.getString()?.apply {
+
+            // default dispatcher for json parsing, cpu intensive work
+            withContext(Dispatchers.Default) {
+              val list = parseJson(this@apply)
+
+              // main dispatcher for interaction with ui objects
+              withContext(Dispatchers.Main) {
+                findViewById<TextView>(R.id.products_data).append("\n\nReading data from json....\n")
+
+                list?.forEach {
+                  findViewById<TextView>(R.id.products_data).append(
+                    "\n${it.packaging}"
+                  )
+                }
+
+              }
+
+            }
+          }
+        }
+      }
+      else{
+        AlertDialog.Builder(this).setTitle("No Internet Connection")
+          .setMessage("Please check your internet connection and try again")
+          .setPositiveButton(android.R.string.ok) { _, _ -> }
+          .setIcon(android.R.drawable.ic_dialog_alert).show()
+      }
+
+    } catch (e: Exception) {
+      e.printStackTrace()
+      apiResponse = e.toString()
+    }
+
+
+
+    val productDataView = findViewById<TextView>(R.id.products_data)
+    productDataView.text = apiResponse
+    return apiResponse
+  }
 
   override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -42,6 +148,7 @@ class MainActivity : AppCompatActivity() {
         .startScan()
         .addOnSuccessListener { barcode: Barcode ->
           barcodeResultView.text = getSuccessfulMessage(barcode)
+          get_product_data(barCodeValue)
         }
         .addOnFailureListener { e: Exception ->
           barcodeResultView.text = getErrorMessage(e as MlKitException)
@@ -73,4 +180,57 @@ class MainActivity : AppCompatActivity() {
       else -> getString(R.string.error_default_message, e)
     }
   }
+
+
+  private fun isNetworkConnected(): Boolean {
+    val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val activeNetwork = connectivityManager.activeNetwork
+    val networkCapabilities = connectivityManager.getNetworkCapabilities(activeNetwork)
+    return networkCapabilities != null &&
+            networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+  }
+
+  // extension function to get string data from url
+  fun URL.getString(): String? {
+    val stream = openStream()
+    return try {
+      val r = BufferedReader(InputStreamReader(stream))
+      val result = StringBuilder()
+      var line: String?
+      while (r.readLine().also { line = it } != null) {
+        result.append(line).appendln()
+      }
+      result.toString()
+    }catch (e: IOException){
+      e.toString()
+    }
+  }
+
+
+  // data class to hold student instance
+  data class Product(
+    val packaging:String
+  )
+
+
+  // parse json data
+  fun parseJson(data:String):List<Product>?{
+    val list = mutableListOf<Product>()
+
+    try {
+      val array = JSONObject(data).getJSONArray("students")
+      //list.add(Product(array1))
+      //val packaging = JSONObject(data).getJSONObject("\"packaging\"").toString()
+      for(i in 0 until array.length()){
+        val obj = JSONObject(array[i].toString())
+        val packaging = obj.getString("firstname")
+      list.add(Product(packaging))
+      }
+    }catch (e: JSONException){
+      Log.d("Exception", e.toString())
+    }
+
+    return list
+  }
+
 }
